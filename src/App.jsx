67656b64aa9792b4a1691b8d987ea73d8d7c9bd0
Shakil Hossain <sh9743441@gmail.com>
2021-08-20 09:36:12 +0600

    rendering movies and deleting item

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
+import Movies from './components/movies';
 
 class App extends Component {
   state = {};
   render() {
-    return <main className='container'>hello there</main>;
+    return (
+      <main className='container pt-4'>
+        <Movies />
+      </main>
+    );
   }
 }
 
