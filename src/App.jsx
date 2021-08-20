23902667fccc7a421b097290f1f3d2f1fd4295c1
Shakil Hossain <sh9743441@gmail.com>
2021-08-20 08:40:55 +0600

    Initialize app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  state = {};
+  render() {
+    return <main className='container'>hello there</main>;
+  }
+}
+
+export default App;
