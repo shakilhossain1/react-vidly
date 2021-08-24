ac698ce764ac555b74475c1e5fffb6db5fb3d076
Shakil Hossain <sh9743441@gmail.com>
2021-08-24 08:52:20 +0600

    complete Routing

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const MovieForm = () => {
-    return ( <h2>Movie Form</h2> );
-}
+const MovieForm = ({match, history}) => {
+  const handleSave = () => {
+    history.push('/movies');
+  };
+  return (
+    <div>
+      <h2>Movie Form - {match.params.id}</h2>
+      <button className='btn btn-primary btn-sm mt-2' onClick={handleSave}>
+        Save
+      </button>
+    </div>
+  );
+};
 
 export default MovieForm;
